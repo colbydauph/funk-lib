21c249efd08d5b53523308e564ac6107db3fce74
Colby Dauphinais <colby@dauphina.is>
2018-11-14 23:09:52 -0800

    better symmetry checking

--- iterable/tests/iterable.test.js
+++ iterable/tests/iterable.test.js
@@ -8,11 +8,7 @@ const async = require('../async');
 describe('iterable', () => {
   
   it('should have symetrical sync / async implementations', () => {
-    expect(
-      Object.keys(sync).length,
-    ).to.eql(
-      Object.keys(async).length,
-    );
+    expect(Object.keys(sync)).to.eql(Object.keys(async));
   });
   
 });

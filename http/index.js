5627b444965c884d8b9285bdaaba4c708b95465a
Colby Dauphinais <colby@dauphina.is>
2018-12-09 15:00:28 -0800

    bumped version

--- http/index.js
+++ http/index.js
@@ -8,13 +8,11 @@ const R = require('ramda');
 const parseContentType = R.pipe(
   R.defaultTo(''),
   R.split(/;\s*/),
-  R.map((str) => {
+  R.chain((str) => {
     const [left, right] = str.split('=');
-    if (!left && !right) return undefined;
-    return right ? [left, right] : ['mimeType', left];
+    if (!left && !right) return [];
+    return right ? [[left, right]] : [['mimeType', left]];
   }),
-  // fixme: should not iterate a second time
-  R.filter(R.identity),
   R.fromPairs
 );
 

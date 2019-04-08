16db0d0c77eee5f4962684d298bd4c1a96d26821
Colby Dauphinais <colby@dauphina.is>
2019-04-07 17:02:22 -0700

    added yieldWith docs

--- src/iterable/yield-with/index.js
+++ src/iterable/yield-with/index.js
@@ -1,6 +1,20 @@
 // modules
 import * as R from 'ramda';
 
+/** Create coroutines with custom behavior by transforming yielded values and
+  * returning them as the results of the yield
+  * @func
+  * @memberof iterable/sync
+  * @sig (a → b) → Iterator<a> → b
+  * @example
+  * const iterator = (function*() {
+  *   const one = yield { val: 1 };
+  *   const two = yield { val: 2 };
+  *   return one + two;
+  * })();
+  * // 3
+  * yieldWith(({ val }) => val, iterator);
+*/
 const yieldWith = R.curry((onYield, iterator) => {
 
   let value, done;
@@ -21,7 +35,22 @@ const yieldWith = R.curry((onYield, iterator) => {
   
 });
 
-// (A → Promise<B>) → AsyncIterator<A> → *
+/** Create coroutines with custom behavior by transforming yielded values and
+  * returning them as the results of the yield. Works with `sync` iterables
+  * @name yieldWith
+  * @memberof iterable/async
+  * @func
+  * @async
+  * @sig (a → Promise<b>) → AsyncIterator<a> → Promise<b>
+  * @example
+  * const iterator = (async function*() {
+  *   const one = yield await { val: 1 };
+  *   const two = yield await { val: 2 };
+  *   return one + two;
+  * })();
+  * // 3
+  * await yieldWith(async ({ val }) => val, iterator);
+*/
 const yieldWithAsync = R.curry(async (onYield, iterator) => {
 
   let value, done;

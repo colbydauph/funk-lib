6dd74d63f6bfb42649154c418145fb66ce68af89
Colby Dauphinais <colby@dauphina.is>
2021-12-30 22:47:34 -0800

    pass correct node version during tests

--- tests/bootstrap.js
+++ tests/bootstrap.js
@@ -6,16 +6,18 @@ const chaiAsPromised = require('chai-as-promised');
 
 // local
 const config = require('../build.config');
-const { name } = require('../package.json');
+const { name, engines } = require('../package.json');
 
 // eslint-disable-next-line no-process-env
 const { NODE_ENV: env } = process.env;
+
+const parseNodeVersion = version => (/[^\d]+(.+)$/.exec(version) || [])[1];
 
 chai.use(chaiAsPromised);
 
 // used by mocha to transpile modules on-the-fly
 require('@babel/register')(config({
   env,
-  node: true,
+  node: parseNodeVersion(engines.node),
   alias: { [name]: './src' },
 }));
